02a16582b4bcb1a1ff20b50e259ea5d9fe3668d4
Szakalopa <maildoalana@gmail.com>
2021-09-22 15:30:21 +0200

    changed from selenium to appium

--- src/Tests/Test001_mobile_checkout.py
+++ src/Tests/Test001_mobile_checkout.py
@@ -1,7 +1,7 @@
 import unittest
 from time import sleep
 
-from selenium import webdriver
+from appium import webdriver
 
 from src.Pages.CheckoutPage import CheckoutPage
 from src.Pages.MainPage import MainPage
@@ -25,18 +25,14 @@ class Test001(unittest.TestCase):
     def test_check_title(self):
         main_page = MainPage(self.driver)
         assert 'MatrixMedia.pl' in main_page.get_title()
-        del main_page
 
         test_product_page = TestProductPage(self.driver)
         test_product_page.add_to_chart()
-        del test_product_page
 
         checkout_page = CheckoutPage(self.driver)
         checkout_page.finalize_order()
-        checkout_page.fill_form()
-        print("checkout_page.pick_delivery_method")
+        checkout_page.fill_form(subject='private')
         checkout_page.pick_delivery_method(DeliveryMethods.KURIER_DHL)
-        del checkout_page
 
     def tearDown(self):
         sleep(15)
